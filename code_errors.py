4c3fadf5a82e5945ac0cfa9efd075969c689e57a
MurVladimirOS <ekliiehxmeh@gmail.com>
2022-11-22 22:46:07 +0300

    feat: transfer filemanager and his dependensy

---
+++ code_errors.py
@@ -0,0 +1,10 @@
+class NotFoundAnalysisError(Exception):
+    """Not found analysis."""
+
+
+class FileIsEmptyError(Exception):
+    """File is empty."""
+
+
+class HeaderNotFoundError(Exception):
+    """File is not header"""
