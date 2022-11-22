3535946942e270840d349601082a37af751234c6
MurVladimirOS <ekliiehxmeh@gmail.com>
2022-11-22 23:48:18 +0300

    feat: add uvicorn

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,3 +8,6 @@ app = FastAPI()
 def root():
     return {"message": "Hello"}
 
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=9090)
