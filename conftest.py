0de83f212360e6d796bde3669a74d2c658366de9
MurVladimirOS <ekliiehxmeh@gmail.com>
2023-01-09 21:30:57 +0300

    fix: fixed fixture for test type analysis

--- conftest.py
+++ conftest.py
@@ -116,13 +116,43 @@ def data_output():
 @pytest.fixture
 def analysis_list():
     return [
-        {'name': 'ABC', 'description': 'some description', 'status': True},
-        {'name': 'XYZ', 'description': 'some description', 'status': False},
-        {'name': 'ABC XYZ', 'description': 'some description', 'status': False},
-        {'name': 'FMR', 'description': 'some description', 'status': False},
-        {'name': 'RFM', 'description': 'some description', 'status': False},
-        {'name': 'FMR RFM', 'description': 'some description', 'status': False},
-        {'name': 'VEN', 'description': 'some description', 'status': False},
-        {'name': 'SWOT', 'description': 'some description', 'status': False},
+    {
+        'name': 'ABC',
+        'description': 'Разделение товаров на три категории по степени их значимости. Поможет определить рентабельные '
+                       'продукты, важных клиентов и поставщиков.',
+        'status': True
+    },
+    {
+        'name': 'XYZ',
+        'description': 'Определение стабильности или устойчивости спроса на товары. Поможет определить какие товары '
+                       'обязательно должны быть на складе или прилавке.',
+        'status': False},
+    {
+        'name': 'ABC XYZ',
+        'description': 'Комбинация анализов которая поможет спланировать закупки. Результатом будет 6 групп которые '
+                       'покажут самые прибыльные и востребованные товары.',
+        'status': False
+    },
+    {
+        'name': 'FMR',
+        'description': 'Анализ выявит насколько товары востребованы. Поможет определить оптимальное место хранения '
+                       'товаров, снизить риск возникновения просроченности, и убрать из ассортимента непопулярную '
+                       'продукцию.',
+        'status': False
+    },
+    {
+        'name': 'RFM',
+        'description': 'Разделение клиентов по частоте и сумме покупок. Поможет выявить тех, кто больше и чаще платит '
+                       'деньги, а кто давно ничего не покупал.',
+        'status': False
+    },
+    {'name': 'FMR RFM', 'description': 'some description', 'status': False},
+    {
+        'name': 'VEN',
+        'description': 'Разделение товаров или ресурсов по степени их важности или необходимости. Поможет определить '
+                       'самые важные запчасти необходимые для производства или  приоритетные лекарственные',
+        'status': False
+    },
+    {'name': 'SWOT', 'description': 'some description', 'status': False},
 
-    ]
+]
