3bd033bc125ced2bfb1d3cf3a50c71565049060d
MurVladimirOS <ekliiehxmeh@gmail.com>
2023-01-16 23:44:39 +0300

    feat: add img in fixture fot test

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
 import pytest
 
+from config import GlobalSettings
+
 
 @pytest.fixture
 def data_result():
@@ -116,43 +118,60 @@ def data_output():
 @pytest.fixture
 def analysis_list():
     return [
-    {
-        'name': 'ABC',
-        'description': 'Разделение товаров на три категории по степени их значимости. Поможет определить рентабельные '
-                       'продукты, важных клиентов и поставщиков.',
-        'status': True
-    },
-    {
-        'name': 'XYZ',
-        'description': 'Определение стабильности или устойчивости спроса на товары. Поможет определить какие товары '
-                       'обязательно должны быть на складе или прилавке.',
-        'status': False},
-    {
-        'name': 'ABC XYZ',
-        'description': 'Комбинация анализов которая поможет спланировать закупки. Результатом будет 6 групп которые '
-                       'покажут самые прибыльные и востребованные товары.',
-        'status': False
-    },
-    {
-        'name': 'FMR',
-        'description': 'Анализ выявит насколько товары востребованы. Поможет определить оптимальное место хранения '
-                       'товаров, снизить риск возникновения просроченности, и убрать из ассортимента непопулярную '
-                       'продукцию.',
-        'status': False
-    },
-    {
-        'name': 'RFM',
-        'description': 'Разделение клиентов по частоте и сумме покупок. Поможет выявить тех, кто больше и чаще платит '
-                       'деньги, а кто давно ничего не покупал.',
-        'status': False
-    },
-    {'name': 'FMR RFM', 'description': 'some description', 'status': False},
-    {
-        'name': 'VEN',
-        'description': 'Разделение товаров или ресурсов по степени их важности или необходимости. Поможет определить '
-                       'самые важные запчасти необходимые для производства или  приоритетные лекарственные',
-        'status': False
-    },
-    {'name': 'SWOT', 'description': 'some description', 'status': False},
+        {
+            'name': 'ABC',
+            'description': 'Разделение товаров на три категории по степени их значимости. Поможет определить рентабельные '
+                           'продукты, важных клиентов и поставщиков.',
+            'status': True,
+            'img': f'{GlobalSettings.Config.BASEDIR}./data/pictures/abc.png'
+        },
+        {
+            'name': 'XYZ',
+            'description': 'Определение стабильности или устойчивости спроса на товары. Поможет определить какие товары '
+                           'обязательно должны быть на складе или прилавке.',
+            'status': False,
+            'img': f'{GlobalSettings.Config.BASEDIR}./data/pictures/XYZ.png'
+        },
+        {
+            'name': 'ABC XYZ',
+            'description': 'Комбинация анализов которая поможет спланировать закупки. Результатом будет 6 групп которые '
+                           'покажут самые прибыльные и востребованные товары.',
+            'status': False,
+            'img': f'{GlobalSettings.Config.BASEDIR}./data/pictures/ABC-XYZ.png'
+        },
+        {
+            'name': 'FMR',
+            'description': 'Анализ выявит насколько товары востребованы. Поможет определить оптимальное место хранения '
+                           'товаров, снизить риск возникновения просроченности, и убрать из ассортимента непопулярную '
+                           'продукцию.',
+            'status': False,
+            'img': f'{GlobalSettings.Config.BASEDIR}./data/pictures/FMR.png'
+        },
+        {
+            'name': 'RFM',
+            'description': 'Разделение клиентов по частоте и сумме покупок. Поможет выявить тех, кто больше и чаще платит '
+                           'деньги, а кто давно ничего не покупал.',
+            'status': False,
+            'img': f'{GlobalSettings.Config.BASEDIR}./data/pictures/RFM.png'
+        },
+        {
+            'name': 'FMR RFM',
+            'description': 'some description',
+            'status': False,
+            'img': f'{GlobalSettings.Config.BASEDIR}./data/pictures/FMR-RFM.png'
+        },
+        {
+            'name': 'VEN',
+            'description': 'Разделение товаров или ресурсов по степени их важности или необходимости. Поможет определить '
+                           'самые важные запчасти необходимые для производства или  приоритетные лекарственные',
+            'status': False,
+            'img': f'{GlobalSettings.Config.BASEDIR}./data/pictures/VEN.png'
+        },
+        {
+            'name': 'SWOT',
+            'description': 'some description',
+            'status': False,
+            'img': f'{GlobalSettings.Config.BASEDIR}./data/pictures/SWOT.png'
+        },
 
-]
+    ]
