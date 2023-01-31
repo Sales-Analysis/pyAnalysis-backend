4c5ccff217f53df4d8b823f24307f1a37a92fb43
MurVladimirOS <ekliiehxmeh@gmail.com>
2023-01-31 19:55:39 +0300

    refactor: extra variable removed

--- api/reports.py
+++ api/reports.py
@@ -18,7 +18,6 @@ async def get_report() -> FileResponse:
 @router.get("/typeAnalysis", response_model=List[AnalysisModel])
 async def get_type_analysis():
     logger.info('get all type analysis')
-    analysis_list = get_analysis_list()
-    result = [AnalysisModel(**i) for i in analysis_list]
+    result = [AnalysisModel(**i) for i in get_analysis_list()]
     logger.info('types of analysis successfully received')
     return result
