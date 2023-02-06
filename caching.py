a9fd0a8ac97e5a839e65063a2792b01c08b76b77
MurVladimirOS <ekliiehxmeh@gmail.com>
2023-02-06 21:12:32 +0300

    draft: lry_cach life time

--- caching.py
+++ caching.py
@@ -4,5 +4,8 @@ from datetime import datetime, timedelta
 
 def lry_cache(seconds: int, maxsize: int = 128):
     def cache(func):
+        func = lru_cache(maxsize=maxsize)(func)
+        func.lifetime = timedelta(seconds=seconds)
+        func.expiration = datetime.utcnow() + func.lifetime
         return
     return cache
