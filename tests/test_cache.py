49ad3235f719cbb1ae4b37b0e36fd02aac64ca20
MurVladimirOS <ekliiehxmeh@gmail.com>
2023-02-06 23:00:42 +0300

    refactor: auxiliary info has been removed

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -14,20 +14,14 @@ def simple_cache(limit: int):
 
 
 def test_cache_lru():
-    print()
     start_time = time.time()
     simple_cache(limit=3)
     finish_time = time.time() - start_time
-    print(f"Finish: {finish_time}")
 
     start_time2 = time.time()
     simple_cache(limit=3)
     finish_time2 = time.time() - start_time2
-    print(f"Finish: {finish_time2}")
 
     assert finish_time2 < finish_time
     assert finish_time2 == 0
 
-    start_time = time.time()
-    simple_cache(limit=2)
-    print(f"Finish: {time.time() - start_time}")
