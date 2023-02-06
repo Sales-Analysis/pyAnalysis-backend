0855c6358bf245d56a5a611761d5e477b0a5e6e2
MurVladimirOS <ekliiehxmeh@gmail.com>
2023-02-06 21:07:20 +0300

    draft: lry_cach

--- caching.py
+++ caching.py
@@ -1,4 +1,8 @@
-from functools import lru_cache
+from functools import lru_cache, wraps
+from datetime import datetime, timedelta
+
 
 def lry_cache(seconds: int, maxsize: int = 128):
-    pass
+    def cache(func):
+        return
+    return cache
