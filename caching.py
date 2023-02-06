fa555382e4c8ab8ae19c08740e121e329ba650e7
MurVladimirOS <ekliiehxmeh@gmail.com>
2023-02-06 21:22:30 +0300

    draft: lry_cach add wrapped

--- caching.py
+++ caching.py
@@ -7,5 +7,14 @@ def lry_cache(seconds: int, maxsize: int = 128):
         func = lru_cache(maxsize=maxsize)(func)
         func.lifetime = timedelta(seconds=seconds)
         func.expiration = datetime.utcnow() + func.lifetime
-        return
+
+        @wraps(func)
+        def wrapped_func(*args, **kwargs):
+            if datetime.utcnow() >= func.expiration:
+                func.cache_clear()
+                func.expiration = datetime.utcnow() + func.lifetime
+                return func(*args, **kwargs)
+
+        return wrapped_func
+
     return cache
